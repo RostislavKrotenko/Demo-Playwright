11b913f446a019cd0bd8cf04fc74324bcc2f984e
RostislavKrotenko <rostik.krotenko@gmail.com>
2025-04-02 12:37:29 +0300

    updated test

--- tests/test_should_fail.py
+++ tests/test_should_fail.py
@@ -1,8 +1,16 @@
 from playwright.sync_api import Page, expect
+import re
 
 def test_should_fail(page: Page):
     page.goto("http://uitestingplayground.com")
 
-    page.get_by_role('link', name='Dynamic ID').click()
-    page.get_by_role('button', name='Button with Dynamic ID').click()
-    
+    page.get_by_role('link', name='Sample App').click()
+
+    label = page.locator('#loginstatus')
+    expect(label).to_contain_text('logged out')
+
+    page.fill('[name="UserName"]', 'Rostyslav')
+    page.fill('[name="Password"]', '12345678')
+    page.get_by_role('button', name='Log In').click()
+
+    expect(label).to_contain_text('fdasdfas')
